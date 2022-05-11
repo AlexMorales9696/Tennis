42b57cdb1fe118c2328e7ec85db74e08c3fddccc
AlexMorales9696 <103995519+AlexMorales9696@users.noreply.github.com>
2022-05-11 17:03:12 +0200

    tuple fattte

---
+++ GameTennis.Test/GameTennis.Test/TennisGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TennisGame
+{
+    
+
+    internal double Punteggio(Tuple<string> tupla)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal double Vantaggio(Tuple<string> tupla)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal double Pareggio(Tuple<string> tupla)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GameTennis.Test/GameTennis.Test/TennisGameTest.cs
@@ -0,0 +1,41 @@
+﻿/*“Love”, “Fifteen”, “Thirty”, and “Forty”*/
+using NUnit.Framework;
+using System;
+
+namespace GameTennis.Test
+{
+    public class TennisGameTest
+    {
+        public  TennisGame _partita;
+        [SetUp]
+        public void Setup()
+    {
+            _partita = new TennisGame();
+    }
+        [TestCase("Forty,Forty", "Deuce")]
+
+        public void Score_Deuce(Tuple<string>input,string expected)
+        {
+           Assert.AreEqual(expected,_partita.Pareggio(input));
+           
+        }
+
+        [TestCase("Forty,4","Advantage,2")]
+        [TestCase("4,Forty", "Advantage,1")]
+        public void Score_Vantaggio(Tuple<string>input,string expected)
+        {
+            Assert.AreEqual(expected, _partita.Vantaggio(input));
+        }
+
+        [TestCase("love,Fifteen", "love,Fifteen")]
+        [TestCase("Fifteen,love", "Fifteen,love")]
+        [TestCase("Thirty,love", "Thirty,love")]
+
+        public void Score_Punteggio(Tuple<string>input, string expected)
+        {
+            Assert.AreEqual(expected, _partita.Punteggio(input));
+        }
+    }
+
+   
+}
